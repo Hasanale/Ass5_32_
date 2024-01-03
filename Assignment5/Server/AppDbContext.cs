6527fe7cf052f9af286cb70a607fa9cefd36e07b
HSN <HSN@DESKTOP-3HSNLA2>
2024-01-03 17:35:33 +0500

    Add project files.

---
+++ Assignment5/Server/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Assignment5.Shared;
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Assignment5.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //configure dbContext option 
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
